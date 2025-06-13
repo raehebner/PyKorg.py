3a5ffcf4879dc26cdd93c04f175d5025d2e64b6c
raehebner <raehebner@gmail.com>
2025-06-13 15:08:26 -0400

    Update atomic_data.py
    added note about the irrelevance of the global part of the global variables

--- src/atomic_data.py
+++ src/atomic_data.py
@@ -1,4 +1,4 @@
-global atomic_symbols
+global atomic_symbols #note: I dont think it actually matters that I make these global variables. 
 atomic_symbols = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
     "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca",
     "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
