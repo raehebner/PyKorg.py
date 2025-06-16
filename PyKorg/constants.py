f75ae0ad699f35d961df2b2364d85895c98470d2
raehebner <raehebner@gmail.com>
2025-06-16 09:28:01 -0400

    Add files via upload

---
+++ PyKorg/constants.py
@@ -0,0 +1,17 @@
+kboltz_cgs = 1.380649e-16 # erg/K
+hplanck_cgs = 6.62607015e-27 # erg*s
+c_cgs = 2.99792458e10 #cm/s
+electron_mass_cgs = 9.1093897e-28 #g
+electron_charge_cgs = 4.80320425e-10 # statcoulomb or cm^3/2 * g^1/2 / s
+amu_cgs = 1.6605402e-24 #g
+bohr_radius_cgs = 5.29177210903e-9 # cm 2018 CODATA recommended value
+
+solar_mass_cgs = 1.9884e33  #g
+G_cgs = 6.67430e-8
+
+eV_to_cgs = 1.602e-12 # ergs per eV
+
+kboltz_eV = 8.617333262145e-5 #eV/K
+hplanck_eV = 4.135667696e-15 #eV*s
+RydbergH_eV = 13.598287264 #eV
+Rydberg_eV = 13.605693122994 #eV 2018 CODATA via wikipedia
